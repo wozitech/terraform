5763b4ad80c4be010cb35d757909ae62e479fcb0
Warren Ayling <warren.ayling@wozitech-ltd.co.uk>
2018-11-24 14:09:43 +0000

    Seriously failing to assign the lambda basic policy across multiple roles.

---
+++ aws/wozitech/variables.tf
@@ -0,0 +1,14 @@
+variable "region" {
+  default = "eu-west-2"
+  description = "The AWS region"
+  type = "string"
+}
+
+variable "name" {
+    default = "WOZiTech"
+}
+
+variable "account" {
+    description = "The id of the AWS account"
+    default = "924132851837"
+}
